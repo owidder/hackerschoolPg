5f95b132ee5b858b9fc36146ec23beb39714ca0c
Oliver Widder <oliver.widder@gmail.com>
2017-03-02 13:33:58 +0100

    with moves

--- js/body.js
+++ js/body.js
@@ -22,26 +22,40 @@ WORLD.rectangleBody = function(cx, cy, width, height, isStatic, color) {
     return rectangle.id;
 };
 
-WORLD.moveBody = function(bodyId, duration, toX, toY) {
-    var finishedPromise = new SimplePromise();
+WORLD.Mover = function(body, duration, toX, toY, promise) {
+    var that = this;
 
-    function oneStep() {
-        var body = Matter.Composite.get(WORLD.engine.world, bodyId, "body");
-        var currentX = body.position.x;
-        var currentY = body.position.y;
-        var _toX = (toX == null ? currentX : toX);
-        var _toY = (toY == null ? currentY : toY);
-        var directionX = Math.sign(_toX - currentX);
-        var directionY = Math.sign(_toY - currentY);
-        if(Math.abs(directionX) < 2 && Math.abs(directionY) < 2) {
-            finishedPromise.resolve();
+    that.toX = toX;
+    that.toY = toY;
+    that.body = body;
+    that.promise = promise;
+    that.duration = duration;
+
+    that.move = function() {
+        var currentX = that.body.position.x;
+        var currentY = that.body.position.y;
+        var diffX = that.toX - currentX;
+        var diffY = that.toY - currentY;
+        var directionX = Math.sign(diffX);
+        var directionY = Math.sign(diffY);
+        if(Math.abs(diffX) < 2 && Math.abs(diffY) < 2) {
+            that.promise.resolve();
         }
         else {
-            Matter.Body.setPosition(body, {x: currentX + directionX, y: currentY + directionY});
+            Matter.Body.setPosition(that.body, {x: currentX + directionX, y: currentY + directionY});
+            setTimeout(that.move, that.duration);
         }
-        setTimeout(oneStep, duration);
-    }
+    };
+};
 
-    oneStep();
+WORLD.moveBody = function(bodyId, duration, toX, toY) {
+    var finishedPromise = new SimplePromise();
+    var body = Matter.Composite.get(WORLD.engine.world, bodyId, "body");
+    var currentX = body.position.x;
+    var currentY = body.position.y;
+    var _toX = (toX == null ? currentX : toX);
+    var _toY = (toY == null ? currentY : toY);
+
+    (new WORLD.Mover(body, duration, _toX, _toY, finishedPromise)).move();
     return finishedPromise.promise;
 };
